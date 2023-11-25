bedd1cff3f39ce8da7c6591a28774c8149b08a98
suryaramesh555 <143591264+suryaramesh555@users.noreply.github.com>
2023-11-25 18:44:13 +0530

    Add files via upload

---
+++ General_Task/Multitwonumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Multitwonumbers{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+System.out.println("enter a first Number");
+int num1 = sc.nextInt();
+System.out.println("enter a second Number");
+int num2 = sc.nextInt();
+int result = num1*num2;
+System.out.println("Multiple of two numbers:"+result);
+}
+}
+
+
+
+
