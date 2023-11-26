1f0de430a5b33bce072c6e8b73655d259e4458e7
suryaramesh555 <143591264+suryaramesh555@users.noreply.github.com>
2023-11-26 11:03:47 +0530

    Delete Task11.java

--- Task11.java
+++
@@ -1,9 +0,0 @@
-class Task11{
-public static void main(String args[]){
-char ch ='s';
-if(ch=='A'||ch=='E'||ch=='i'||ch=='o'||ch=='u')
-System.out.println(ch +" is vowel");
-else
-System.out.println(ch +" is consonant");
-}
-}
